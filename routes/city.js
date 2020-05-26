5ca9499838e82d332babbd107791646d0349f4ce
redsea-tours <m.khatab.88@gmail.com>
2020-05-26 22:24:28 +0200

    add cities get and post routes

--- routes/city.js
+++ routes/city.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-
 const CityService = require("../core/services/city-service");
-
 const citySvc = new CityService();
 
 router.get("/cities", (req, res, next) => {
-    res.json(citySvc.get());
+    const cities = citySvc.get();
+    res.json(cities);
+});
+
+router.get("/cities/:id", (req, res, next) => {
+    const id = req.params.id;
+    const city = citySvc.get().find(c => c.id === id);
+
+    if(!city) {
+        res.status(404).json("Not found!");
+        return;
+    }
+
+    res.json(city);
+});
+
+router.post("/cities", (req, res, next) => {
+    const body = req.body;
+    res.json(body);
 });
 
 module.exports = router;
