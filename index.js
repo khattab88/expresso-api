81bada0f946316ca39ad6b830ae878596fca5f34
khattab88 <m_khatab_88@hotmail.com>
2020-05-27 20:59:56 +0200

    deploy 2

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// });
 
 // app.use(cityRoutes);
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
     res.send("hello expresso!");
 });
 
