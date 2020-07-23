9e0e087a5945700b50ca47871f68b1d917fcc6af
khattab88 <m_khatab_88@hotmail.com>
2020-07-23 04:40:34 +0200

    implement global error handling middleware

--- app.js
+++ app.js
@@ -43,10 +43,27 @@ app.get("/", (req, res) => {
 });
 
 /* FALLBACK ROUTE */
-app.all("*", (req, res) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Can't find ${req.originalUrl} on this server!`
+app.all("*", (req, res, next) => {
+    // res.status(404).json({
+    //     status: "fail",
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.statusCode = 404;
+    err.status = "fail";
+
+    next(err);
+});
+
+/* GLOBAL ERROR HANDLING MIDDLEWARE */
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     });
 });
 
