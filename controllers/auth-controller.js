43a98f0daa336abf02e36bc6845c804e3831002e
khattab88 <m_khatab_88@hotmail.com>
2020-07-27 18:00:25 +0200

    generate jwt tokens

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,14 +3,27 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable prettier/prettier */
+const jwt = require('jsonwebtoken');
+
 const userRepo = require("../repositories/user-repository");
 const catchAsync = require("../utils/catch-async");
 
 exports.signup = catchAsync(async (req, res) => {
-    const newUser = await userRepo.create(req.body);
+    const newUser = await userRepo.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES
+    });
 
     res.status(201).json({
         status: 'success',
+        token,
         data: { user: newUser }
     });
 });
