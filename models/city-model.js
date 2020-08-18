e2b01134400f4e4208652bdac7c6f99446dae304
khattab88 <m_khatab_88@hotmail.com>
2020-08-18 08:44:00 +0200

    Revert "add slug field"
    This reverts commit 54d9931dcfe48d7053ad6c479a6bc091ad43ba07.

--- models/city-model.js
+++ models/city-model.js
@@ -1,8 +1,6 @@
 /* eslint-disable prettier/prettier */
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
-const slugify = require('slugify');
-const validator = require('validator');
 
 const citySchema = new mongoose.Schema({
     id: {
@@ -16,14 +14,12 @@ const citySchema = new mongoose.Schema({
         trim: true,
         unique: true,
     },
-    slug: {
-        type: String,
-        validate: {
-            validator: (val) => validator.isSlug(val),
-            message: "slug ({VALUE}) is not a valid slug!"
-        }
-    },
     country: {
+        // id: {
+        //     type: String,
+        //     default: uuidv4,
+        //     unique: true,
+        // },
         name: {
             type: String,
             required: [true, 'Country must have a name!'],
@@ -45,16 +41,6 @@ const citySchema = new mongoose.Schema({
             }
         }
     ]
-});
-
-// Document middleware: runs BEFORE .save() and .create()
-citySchema.pre("save", function(next) {
-    // eslint-disable-next-line prefer-destructuring
-    this.slug = slugify(this.name, {
-        lower: true,
-        remove: /[*+~.()'"!:@]/g 
-    });
-    next();
 });
 
 const City = mongoose.model("city", citySchema);
