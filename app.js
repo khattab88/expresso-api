68ac89ca76bc653fdfc1a3da147ce54d724b6cd7
khattab88 <m_khatab_88@hotmail.com>
2021-01-27 15:38:50 +0200

    add response compression middleware

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xssClean = require("xss-clean");
 const hpp = require("hpp");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
 const AppError = require("./utils/app-error");
 const globalErorrHandler = require("./controllers/error-controller");
@@ -100,6 +101,8 @@ app.use((req, res, next) => {
     next();
 });
 
+// enable response compression
+app.use(compression());
 
 // Test Middleware
 app.use((req, res, next) => {
