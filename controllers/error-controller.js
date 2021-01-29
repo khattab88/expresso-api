57f717c41f0fcbca8e5b1bffba7961251773e521
khattab88 <m_khatab_88@hotmail.com>
2021-01-29 09:58:28 +0200

    update sendErrorProd

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -50,8 +50,10 @@ const sendErrorProd = (err, res) => {
         console.error("ERROR!", err);
         
         res.status(500).json({
-            status: "error",
-            message: "Something went wrong!"
+            // status: "error",
+            // message: "Something went wrong!"
+            status: err.status,
+            message: err.message
         });
     }
 };
