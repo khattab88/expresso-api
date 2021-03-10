6a3af9a611268a5a423291890eac6cdfefa11005
khattab88 <m_khatab_88@hotmail.com>
2021-03-10 22:05:06 +0200

    include areas of city in get all cities

--- controllers/city-controller.js
+++ controllers/city-controller.js
@@ -2,15 +2,22 @@
 /* eslint-disable prettier/prettier */
 
 // const City = require("../models/city-model");
-// const { City } = require('expresso-models');
+const { City, Area } = require('expresso-models');
 // const cityRepo = require("../repositories/city-repository");
-const { cityRepository: cityRepo } = require("expresso-repositories");
+const { cityRepository: cityRepo, areaRepository: areaRepo } = require("expresso-repositories");
 
 const catchAsync = require("../utils/catch-async");
 const AppError = require("../utils/app-error");
 
 const controllerFactory = require("./controller-factory");
 
+
+exports.setCountryId = (req, res, next) => {
+    // allow nested rotes
+    if(!req.body.country) req.body.country = req.params.countryId;
+    
+    next();
+};
 
 exports.getAllCities = catchAsync(async (req, res, next) => {
     let cities = null;
@@ -20,6 +27,21 @@ exports.getAllCities = catchAsync(async (req, res, next) => {
     } else {
         cities = await cityRepo.getAll();
     }
+
+    cities = [...cities].map(c => {
+        return {
+            _id: c._id,
+            id: c.id,
+            name: c.name
+        }
+    });
+
+    for(let city of cities) {
+        // city.areas = await areaRepo.getByCityId(city._id);
+        city.areas = await Area.find({ "city": city._id });
+    }
+
+    // console.log(cities);
     
     res.status(200).json({
         status: "success",
@@ -29,13 +51,6 @@ exports.getAllCities = catchAsync(async (req, res, next) => {
 });
 
 exports.getCity = controllerFactory.getById(cityRepo);
-
-exports.setCountryId = (req, res, next) => {
-    // allow nested rotes
-    if(!req.body.country) req.body.country = req.params.countryId;
-    
-    next();
-};
 
 exports.createCity = controllerFactory.create(cityRepo);
 
